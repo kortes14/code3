6232edcffc2888e4d90b0ed568f36629b65d15a5
kortes14 <jakub.kertys@gmail.com>
2013-12-11 10:07:58 +0100

    pridal som cpp

---
+++ cviko9.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int vypocet_velkosti() {
+	int velkost = 0;
+
+
+	return 0;
+}
+//komentaris
+void main() {
+	
+	FILE *f;
+	f = fopen("subor.txt","r");
+	//fprintf(f,"ahoooj");
+	
+
+
+	//printf("%s",f);
+	fclose(f);
+
+	printf("sulin");
+
+	system("pause");
+
+
+}
